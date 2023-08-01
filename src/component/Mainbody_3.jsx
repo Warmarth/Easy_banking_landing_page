5cb6d5cef329a1aaaba3f5c7b382eab30b5174a4
Warmarth <godsonibeaka@gmail.com>
2023-08-01 11:03:59 +0100

    update

--- src/component/Mainbody_3.jsx
+++ src/component/Mainbody_3.jsx
@@ -1,7 +1,14 @@
 import react from "react";
+import "../App.css";
 
 const Mainbody_3 = () => {
-  return <section></section>;
+  return (
+    <section>
+      <div>
+       
+      </div>
+    </section>
+  );
 };
 
 export default Mainbody_3;
